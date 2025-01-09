fb8f639051a3b4a4e65919ee6b36fba43d0ac27a
Dewakayika <kayikadewa@gmail.com>
2025-01-09 16:24:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
